269f211bb28d76a650a8013f793502df3ac98fa9
Mohammed Dadi <artofmohammed@gmail.com>
2025-06-14 00:28:14 +0100

    feat: add --verbose flag for detailed CLI output
    - Shows user prompt, prompt tokens, and response tokens when enabled
    - Keeps output clean when flag is omitted

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from google import genai
+from google import genai 
+from google.genai import types
 from dotenv import load_dotenv
 import sys
 
@@ -8,23 +9,41 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 command_args = sys.argv
+
+
 try: 
-    arg = command_args[1]
+    user_prompt = command_args[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 except Exception as e:
     print('failed to retrieve argument, error: ', e)
     sys.exit(1)
 
 try:
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents= arg 
+        model='gemini-2.0-flash-001', contents= messages,
     )
 except Exception as e:
     print("request to model failed, error: ", e)
     sys.exit(1)
     
+
+
+
 prompt_token = response.usage_metadata.prompt_token_count
 response_token = response.usage_metadata.candidates_token_count
 
+#check if the --verbose flag was passed by the user
+# if len(command_args) == 3:
+#     (print(f'User prompt: {user_prompt}\nGemini: {response.text}\nPrompt tokens: {prompt_token}\nResponse tokens: {response_token}'), None)[command_args[2] == "--verbose"]
 
-print(f'Gemini: {response.text}')
-print(f'Prompt tokens: {prompt_token}\nResponse tokens: {response_token}')   
+if len(command_args) == 3:
+    if command_args[2] == "--verbose":
+        print(f'User prompt: {user_prompt}')
+        print(f'Gemini: {response.text}')
+        print(f'Response tokens: {response_token}')
+        print(f'Prompt tokens: {prompt_token}')
+        
+
+    
