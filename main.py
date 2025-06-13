247b80678db2d0c666520c3139b4ff154f748a1a
Mohammed Dadi <artofmohammed@gmail.com>
2025-06-13 20:45:49 +0100

    feat: Accept prompt as command-line argument
    This allows users to specify the prompt directly when running the script,
    rather than having to modify the code.
    
    Also includes basic error handling for missing prompts.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-
+command_args = sys.argv
+try: 
+    arg = command_args[1]
+except Exception as e:
+    print('failed to retrieve argument, error: ', e)
+    sys.exit(1)
 
 try:
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.' 
+        model='gemini-2.0-flash-001', contents= arg 
     )
 except Exception as e:
     print("request to model failed, error: ", e)
+    sys.exit(1)
     
 prompt_token = response.usage_metadata.prompt_token_count
 response_token = response.usage_metadata.candidates_token_count
